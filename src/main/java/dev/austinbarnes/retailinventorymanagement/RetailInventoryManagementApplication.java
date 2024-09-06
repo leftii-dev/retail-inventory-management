c0a764f3f1d8fc1d2e0ff75d839c983041da3a38
Austin Barnes <austin@austinbarnes.dev>
2024-09-06 09:48:12 -0700

    Initial Commit

---
+++ src/main/java/dev/austinbarnes/retailinventorymanagement/RetailInventoryManagementApplication.java
@@ -0,0 +1,13 @@
+package dev.austinbarnes.retailinventorymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailInventoryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailInventoryManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/austinbarnes/retailinventorymanagement/RetailInventoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.austinbarnes.retailinventorymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailInventoryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
