3aa102e1344b1604d79d8b3c415fbb3911deb06d
Austin Barnes <austin@austinbarnes.dev>
2024-09-22 07:02:19 -0700

    Product entity: -Create Fields -Add Validation

---
+++ src/main/java/dev/austinbarnes/retailinventorymanagement/product/Product.java
@@ -0,0 +1,93 @@
+package dev.austinbarnes.retailinventorymanagement.product;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.type.SqlTypes;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.Map;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "product")
+public class Product {
+    @Id
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "sku", nullable = false, unique = true)
+    @Max(20)
+    private String sku;
+
+    @Column(name = "product_code")
+    @Max(10)
+    private String productCode;
+
+    @Column(name = "name", nullable = false)
+    @NotNull
+    @Max(100)
+    @Min(5)
+    private String name;
+
+    @Column(name = "description")
+    @Max(1500)
+    private String description;
+
+    @Column(name = "price")
+    @DecimalMax("9999999999.99")
+    private BigDecimal price;
+
+    @Column(name = "weight")
+    @DecimalMax("999999.99")
+    private BigDecimal weight;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(name = "dimensions", columnDefinition = "jsonb")
+    private Map<String, Object> dimensions;
+
+    @Column(name = "created_at", updatable = false, columnDefinition = "timestamptz")
+    @CreationTimestamp
+    private Instant createdAt;
+
+    @Column(name = "modified_at", columnDefinition = "timestamptz")
+    @UpdateTimestamp
+    private Instant modifiedAt;
+
+    @Column(name = "is_active")
+    private Boolean isActive;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", referencedColumnName = "id")
+    private Category category;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "created_by_id", referencedColumnName = "id")
+    private Employee createdBy;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "modified_by_id", referencedColumnName = "id")
+    private Employee modifiedBy;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "brand_id", referencedColumnName = "id")
+    private Brand brand;
+
+    @Column(name = "deleted")
+    private Boolean deleted;
+}
