6411978872439d2d80f64191d2defc58a33c4380
Austin Barnes <austin@austinbarnes.dev>
2024-10-07 07:13:24 -0700

    Update Comments - Brand DTOs

--- src/main/java/dev/austinbarnes/retailinventorymanagement/product/dto/brand/BrandRequestDTO.java
+++ src/main/java/dev/austinbarnes/retailinventorymanagement/product/dto/brand/BrandRequestDTO.java
@@ -1,5 +1,8 @@
 package dev.austinbarnes.retailinventorymanagement.product.dto.brand;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 /**
  * Brand Request DTO for creating and updating Brand entries
  *
@@ -10,7 +13,7 @@ package dev.austinbarnes.retailinventorymanagement.product.dto.brand;
  * @since 2024
  * */
 public record BrandRequestDTO(
-    String name,
-    String description
+    @Size(min = 1, max = 50, message = "Brand name must be between 1 and 50 characters") @NotNull String name,
+    @Size(max = 3000, message = "Description cannot exceed 3000 characters") String description
 ) {
 }
