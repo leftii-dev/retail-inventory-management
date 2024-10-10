b629aae5bf9d1b2f378ceed50615c75507b8aa1e
Austin Barnes <austin@austinbarnes.dev>
2024-10-10 11:01:16 -0700

    Create BrandResponse DTOs

--- src/main/java/dev/austinbarnes/retailinventorymanagement/product/dto/brand/BrandResponseClientDTO.java
+++ src/main/java/dev/austinbarnes/retailinventorymanagement/product/dto/brand/BrandResponseClientDTO.java
@@ -3,19 +3,17 @@ package dev.austinbarnes.retailinventorymanagement.product.dto.brand;
 import java.util.UUID;
 
 /**
- * Brand Response Detail DTO for Admin panel
+ * Brand Response DTO for end-user
+ *
  * @param id
  * @param name
  * @param description
- * @param deleted
- *
  * @author Austin Barnes
  * @since 2024
- * */
-public record BrandResponseDetailDTO(
+ */
+public record BrandResponseClientDTO(
         UUID id,
         String name,
-        String description,
-        boolean deleted
-) {
+        String description
+) implements BrandResponseDTO {
 }
--- src/main/java/dev/austinbarnes/retailinventorymanagement/product/dto/brand/BrandResponseDTO.java
+++ src/main/java/dev/austinbarnes/retailinventorymanagement/product/dto/brand/BrandResponseDTO.java
@@ -3,18 +3,16 @@ package dev.austinbarnes.retailinventorymanagement.product.dto.brand;
 import java.util.UUID;
 
 /**
- * Brand Response DTO for end-user
- *
- * @param id
- * @param name
- * @param description
+ * DTO interface for Products
+ * Allows return type safety in controller with different DTO records
  *
  * @author Austin Barnes
  * @since 2024
- * */
-public record BrandResponseDTO(
-        UUID id,
-        String name,
-        String description
-) {
+ */
+public interface BrandResponseDTO {
+    UUID id();
+
+    String name();
+
+    String description();
 }
