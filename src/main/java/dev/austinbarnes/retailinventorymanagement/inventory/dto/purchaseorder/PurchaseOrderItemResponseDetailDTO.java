704811bbb88774a1c7bab9a348ced56adb567d8d
Austin Barnes <austin@austinbarnes.dev>
2024-10-11 11:17:28 -0700

    Create PurchaseOrderItem DTOs

---
+++ src/main/java/dev/austinbarnes/retailinventorymanagement/inventory/dto/purchaseorder/PurchaseOrderItemResponseDetailDTO.java
@@ -0,0 +1,22 @@
+package dev.austinbarnes.retailinventorymanagement.inventory.dto.purchaseorder;
+
+import dev.austinbarnes.retailinventorymanagement.product.dto.product.ProductResponseDTO;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
+public record PurchaseOrderItemResponseDetailDTO(
+        UUID id,
+        BigDecimal costUnit,
+        BigDecimal costLineTotal,
+        short quantity,
+        Instant createdAt,
+        Instant modifiedAt,
+        PurchaseOrderResponseDTO purchaseOrder,
+        ProductResponseDTO product,
+        EmployeeResponseDTO createdBy,
+        EmployeeResponseDTO modifiedBy,
+        boolean deleted
+) implements PurchaseOrderItemResponseDTO{
+}
