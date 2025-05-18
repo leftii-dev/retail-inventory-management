8a8af3cfc84bbb6c6f1de570078d48536a52f249
leftii-dev <austin@austinbarnes.dev>
2025-05-18 15:50:10 -0700

    Create javadoc comments for Status DTOs

--- src/main/java/dev/austinbarnes/retailinventorymanagement/inventory/dto/status/StatusResponseBasicDTO.java
+++ src/main/java/dev/austinbarnes/retailinventorymanagement/inventory/dto/status/StatusResponseBasicDTO.java
@@ -2,6 +2,13 @@ package dev.austinbarnes.retailinventorymanagement.inventory.dto.status;
 
 import java.util.UUID;
 
+/**
+ * StatusResponseBasicDTO is a Data Transfer Object (DTO) used for transferring basic status data.
+ * <p>
+ * It contains fields for status ID, name, and description.
+ * <p>
+ * This DTO is used to represent the status data in a simplified format.
+ */
 public record StatusResponseBasicDTO(
         UUID id,
         String name,
