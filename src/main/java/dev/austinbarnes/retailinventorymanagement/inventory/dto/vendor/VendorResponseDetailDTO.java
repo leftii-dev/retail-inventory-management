15abae390edb954fbf44e1b2e9452925ee99b98b
leftii-dev <austin@austinbarnes.dev>
2025-05-18 15:53:57 -0700

    Create javadoc comments for Vendor DTOs

--- src/main/java/dev/austinbarnes/retailinventorymanagement/inventory/dto/vendor/VendorResponseDetailDTO.java
+++ src/main/java/dev/austinbarnes/retailinventorymanagement/inventory/dto/vendor/VendorResponseDetailDTO.java
@@ -3,6 +3,13 @@ package dev.austinbarnes.retailinventorymanagement.inventory.dto.vendor;
 import java.time.Instant;
 import java.util.UUID;
 
+/**
+ * VendorResponseDetailDTO is a Data Transfer Object (DTO) used for transferring detailed vendor data.
+ * <p>
+ * It contains fields for vendor ID, vendor code, name, address, contact information, email, and status.
+ * <p>
+ * This DTO is used to represent the vendor data in a detailed format.
+ */
 public record VendorResponseDetailDTO(
         UUID id,
         String vendorCode,
