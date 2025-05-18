68ef58bb9e84685d17b495c5ed2b70d7a2992b27
leftii-dev <austin@austinbarnes.dev>
2025-05-18 16:02:21 -0700

    Create javadoc comments for Inventory related mappers

--- src/main/java/dev/austinbarnes/retailinventorymanagement/inventory/mapper/PurchaseOrderItemMapper.java
+++ src/main/java/dev/austinbarnes/retailinventorymanagement/inventory/mapper/PurchaseOrderItemMapper.java
@@ -10,15 +10,41 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 
+/**
+ * PurchaseOrderItemMapper is an interface that defines the mapping between PurchaseOrderItem entity and its DTOs.
+ * <p>
+ * It uses MapStruct to generate the implementation of the mapping methods.
+ * <p>
+ * The interface includes methods to convert PurchaseOrderItemRequestDTO to PurchaseOrderItem entity and
+ * to convert PurchaseOrderItem entity to different types of PurchaseOrderItemResponseDTOs.
+ */
 @Mapper(config = GlobalMapperConfig.class, uses = {ProductMapper.class})
 public interface PurchaseOrderItemMapper {
+    /**
+     * Converts PurchaseOrderItemRequestDTO to PurchaseOrderItem entity.
+     *
+     * @param purchaseOrderItemRequestDTO the PurchaseOrderItemRequestDTO to convert
+     * @return the converted PurchaseOrderItem entity
+     */
     PurchaseOrderItem toEntity(PurchaseOrderItemRequestDTO purchaseOrderItemRequestDTO);
 
+    /**
+     * Converts PurchaseOrderItem entity to PurchaseOrderItemResponseBasicDTO.
+     *
+     * @param purchaseOrderItem the PurchaseOrderItem entity to convert
+     * @return the converted PurchaseOrderItemResponseBasicDTO
+     */
     @Mapping(target = "product", qualifiedByName = "basicProduct")
     @Mapping(target = "purchaseOrderID", source = "purchaseOrder.id")
     @Named("basicPurchaseOrderItem")
     PurchaseOrderItemResponseBasicDTO toBasicDTO(PurchaseOrderItem purchaseOrderItem);
 
+    /**
+     * Converts PurchaseOrderItem entity to PurchaseOrderItemResponseDetailDTO.
+     *
+     * @param purchaseOrderItem the PurchaseOrderItem entity to convert
+     * @return the converted PurchaseOrderItemResponseDetailDTO
+     */
     @Mapping(target = "purchaseOrderID", source = "purchaseOrder.id")
     @Mapping(target = "product", qualifiedByName = "detailProduct")
     @Named("detailPurchaseOrderItem")
--- src/main/java/dev/austinbarnes/retailinventorymanagement/inventory/mapper/TransferMapper.java
+++ src/main/java/dev/austinbarnes/retailinventorymanagement/inventory/mapper/TransferMapper.java
@@ -9,19 +9,45 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 
+/**
+ * TransferMapper is an interface that defines the mapping between Transfer entity and its DTOs.
+ * <p>
+ * It uses MapStruct to generate the implementation of the mapping methods.
+ * <p>
+ * The interface includes methods to convert TransferRequestDTO to Transfer entity and
+ * to convert Transfer entity to different types of TransferResponseDTOs.
+ */
 @Mapper(config = GlobalMapperConfig.class)
 public interface TransferMapper {
 
+    /**
+     * Converts TransferRequestDTO to Transfer entity.
+     *
+     * @param transferRequestDTO the TransferRequestDTO to convert
+     * @return the converted Transfer entity
+     */
     // TODO: Once repos and services are created, add LocationService to `uses` and inject to retrieve locationTo/From
     @Mapping(target = "locationTo", ignore = true) // Remove after injection complete
     @Mapping(target = "locationFrom", ignore = true) // Remove after injection complete
     Transfer toEntity(TransferRequestDTO transferRequestDTO);
 
+    /**
+     * Converts Transfer entity to TransferResponseBasicDTO.
+     *
+     * @param transfer the Transfer entity to convert
+     * @return the converted TransferResponseBasicDTO
+     */
     @Mapping(target = "locationToID", source = "locationTo.id")
     @Mapping(target = "locationFromID", source = "locationFrom.id")
     @Named("basicTransfer")
     TransferResponseBasicDTO toBasicDTO(Transfer transfer);
 
+    /**
+     * Converts Transfer entity to TransferResponseDetailDTO.
+     *
+     * @param transfer the Transfer entity to convert
+     * @return the converted TransferResponseDetailDTO
+     */
     @Mapping(target = "locationToID", source = "locationTo.id")
     @Mapping(target = "locationFromID", source = "locationFrom.id")
     @Named("detailTransfer")
