9fce446fd7a06d2ef020bc14a204a61e5ca59905
leftii-dev <austin@austinbarnes.dev>
2025-05-19 07:05:38 -0700

    Create javadoc comments for  Product related Mappers

--- src/main/java/dev/austinbarnes/retailinventorymanagement/product/mapper/DiscountMapper.java
+++ src/main/java/dev/austinbarnes/retailinventorymanagement/product/mapper/DiscountMapper.java
@@ -6,16 +6,39 @@ import dev.austinbarnes.retailinventorymanagement.product.dto.discount.DiscountR
 import dev.austinbarnes.retailinventorymanagement.product.dto.discount.DiscountResponseDetailDTO;
 import dev.austinbarnes.retailinventorymanagement.product.entity.Discount;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 
+/**
+ * DiscountMapper is an interface that defines the mapping between the Discount entity and its corresponding DTOs.
+ * It uses MapStruct to generate the implementation at compile time.
+ * The mappings include converting a DiscountRequestDTO to a Discount entity and converting a Discount entity to
+ * both a basic and detailed DiscountResponseDTO.
+ */
 @Mapper(config = GlobalMapperConfig.class)
 public interface DiscountMapper {
+    /**
+     * Converts a DiscountRequestDTO to a Discount entity.
+     *
+     * @param discountRequestDTO the DiscountRequestDTO to convert
+     * @return the converted Discount entity
+     */
     Discount toEntity(DiscountRequestDTO discountRequestDTO);
 
+    /**
+     * Converts a Discount entity to a DiscountResponseBasicDTO.
+     *
+     * @param discount the Discount entity to convert
+     * @return the converted DiscountResponseBasicDTO
+     */
     @Named("basicDiscount")
     DiscountResponseBasicDTO toBasicDTO(Discount discount);
 
+    /**
+     * Converts a Discount entity to a DiscountResponseDetailDTO.
+     *
+     * @param discount the Discount entity to convert
+     * @return the converted DiscountResponseDetailDTO
+     */
     @Named("detailDiscount")
     DiscountResponseDetailDTO toDetailDTO(Discount discount);
 }
